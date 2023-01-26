7308de12b2b38cbbf72b37cdefbd659923ebd6b0
Emmanuel Doré-Walsh <emmanuel.dore.walsh@hotmail.com>
2023-01-26 14:51:48 -0500

    Ajout projet TP1

---
+++ TP1/Assets/Components/Cercle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cerlces
+{
+    struct Entity
+    {
+        int id;
+    }
+
+    struct Taille
+    {
+        int diametre;
+    }
+
+    struct Vitesse
+    { 
+        Vector2 vitesse;
+    }
+
+    struct Possition
+    {
+        Vector2 position;
+    }
+
+    struct Couleur
+    {
+        Color couleur;
+    }
+
+    
+
+}
