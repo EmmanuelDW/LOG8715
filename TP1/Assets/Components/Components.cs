87dc1617ad6529da2922f664421eb4ffb81d40d6
unknown <alex.hua@polymtl.ca>
2023-01-30 16:59:24 -0500

    ajout circle test

---
+++ TP1/Assets/Components/Components.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Components
+{
+    struct Entity
+    {
+        int id;
+    }
+
+    struct Position
+    {
+        Vector2 position;
+    }
+
+    struct Scale
+    {
+        int radius;
+    }
+
+    struct Velocity
+    {
+        Vector2 velocity;
+    }
+}
