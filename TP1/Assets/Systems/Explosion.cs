cfa79e85e0ca0c1ebae624ba1c7de03b78cde3e2
unknown <alex.hua@polymtl.ca>
2023-02-16 13:10:40 -0500

    explosion fonctionne avec click

--- TP1/Assets/Systems/Explosion.cs
+++ TP1/Assets/Systems/Explosion.cs
@@ -17,11 +17,88 @@ public class Explosion : ISystem
     float startTime;
     public void UpdateSystem()
     {
+        Dictionary<uint, Position> position = new Dictionary<uint, Position>(Composante.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("click2");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            //Debug.Log(ray);
+            Vector2 pos = (Vector2)ray.origin;
+            Debug.Log(pos);
+            foreach (KeyValuePair<uint, Position> p in position)
+            {
+                if (pos.x <= p.Value.position.x + (Composante.taille[p.Key].taille / 2)
+                    && pos.x >= p.Value.position.x - (Composante.taille[p.Key].taille / 2)
+                    && pos.y <= p.Value.position.y + (Composante.taille[p.Key].taille / 2)
+                    && pos.y >= p.Value.position.y - (Composante.taille[p.Key].taille / 2))
+                {
+                    Debug.Log(p.Key);
+                    if (Composante.taille[p.Key].taille >= 2)
+                    {
+                        Debug.Log(Composante.taille[p.Key].taille);
+                        Vitesse v = Composante.vitesse[p.Key];
+                        Hit h;
+                        h.hit = false;
+                        Click c;
+                        c.click = false;
+                        Protection protection;
+                        protection.cooldown = 0f;
+                        protection.timeleft = 0f;
+                        Vitesse v2;
+                        v2.vitesse.x = -(v.vitesse.x);
+                        v2.vitesse.y = -(v.vitesse.y);
+                        Couleur couleur;
+                        couleur.couleur = Color.blue;
+
+                        uint firstCircleId = StartUp.count;
+                        StartUp.count++;
+
+                        uint secondCircleId = StartUp.count;
+                        StartUp.count++;
+
+                        Taille taille1;
+                        taille1.taille = Composante.taille[p.Key].taille / 2;
+                        Composante.taille.Add(firstCircleId, taille1);
+                        Composante.position.Add(firstCircleId, p.Value);
+                        Composante.protection.Add(firstCircleId, protection);
+                        Composante.vitesse.Add(firstCircleId, v);
+                        Composante.hit.Add(firstCircleId, h);
+                        Composante.couleur.Add(firstCircleId, couleur);
+                        Composante.click.Add(firstCircleId, c);
+                        manager.CreateShape(firstCircleId, (int)taille1.taille);
+                        manager.UpdateShapePosition(firstCircleId, p.Value.position);
+
+
+                        Taille taille2;
+                        taille2.taille = Composante.taille[p.Key].taille / 2;
+                        Composante.taille.Add(secondCircleId, taille2);
+                        Composante.position.Add(secondCircleId, p.Value);
+                        Composante.protection.Add(secondCircleId, protection);
+                        Composante.vitesse.Add(secondCircleId, v2);
+                        Composante.hit.Add(secondCircleId, h);
+                        Composante.couleur.Add(secondCircleId, couleur);
+                        Composante.click.Add(secondCircleId, c);
+                        manager.CreateShape(secondCircleId, (int)taille2.taille);
+                        manager.UpdateShapePosition(secondCircleId, p.Value.position);
+
+
+                    }
+                    manager.DestroyShape(p.Key);
+                    Composante.taille.Remove(p.Key);
+                    Composante.position.Remove(p.Key);
+                    Composante.vitesse.Remove(p.Key);
+                    Composante.couleur.Remove(p.Key);
+                    Composante.protection.Remove(p.Key);
+                    Composante.hit.Remove(p.Key);
+                    Composante.click.Remove(p.Key);
+
+                }
+            }
+        }
 
         Dictionary<uint, Taille> taille = new Dictionary<uint, Taille>(Composante.taille);
         //Debug.Log("Explosion");
-
-        
         foreach (KeyValuePair<uint, Taille> i in taille)
         {
  
@@ -29,7 +106,6 @@ public class Explosion : ISystem
                 //Debug.Log(explosionSize);
                 if (i.Value.taille == explosionSize)
                 {
-
 
                     Position p = Composante.position[i.Key];
                     Vitesse v = Composante.vitesse[i.Key];
