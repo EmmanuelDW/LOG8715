81319e5d749a240f179c6f55db851dd0907d4242
unknown <alex.hua@polymtl.ca>
2023-03-19 22:06:10 -0400

    ajout job system

--- TP2/Assets/Ex4/Scripts/ChangePlantLifetime.cs
+++ TP2/Assets/Ex4/Scripts/ChangePlantLifetime.cs
@@ -10,16 +10,14 @@ struct ChangePlantLifeTimeJob : IJobParallelFor
     public NativeArray<Vector3> preyTransformsPosition;
     public Vector3 transformPosition;
     public float touchingDistance;
-    //public Lifetime lifetime;
     public float decreasingFactor;
 
     public void Execute(int index)
     {
         if (Vector3.Distance(preyTransformsPosition[index], transformPosition) < touchingDistance)
         {
-            // lifetime.decreasingFactor *= 2f;
             decreasingFactor *= 2f;
-        }
+        } 
     }
 }
 
@@ -35,44 +33,28 @@ public class ChangePlantLifetime : MonoBehaviour
 
     public void Update()
     {
-        // _lifetime.decreasingFactor = 1.0f;
-        //foreach (var prey in Ex4Spawner.PreyTransforms)
-        //{
-        //    if (Vector3.Distance(prey.position, transform.position) < Ex4Config.TouchingDistance)
-        //    {
-        //        _lifetime.decreasingFactor *= 2f;
-        //        break;
-        //    }
-
-        //}
         _lifetime.decreasingFactor = 1.0f;
-        Vector3[] preyPositions = new Vector3[Ex4Spawner.PreyTransforms.Length];
+
+
+        NativeArray<Vector3> preyTranformPositionArray = new NativeArray<Vector3>(Ex4Spawner.PreyTransforms.Length, Allocator.TempJob);
         for (int i = 0; i < Ex4Spawner.PreyTransforms.Length; i++)
         {
-            preyPositions[i] = Ex4Spawner.PreyTransforms[i].position;
+            preyTranformPositionArray[i] = Ex4Spawner.PreyTransforms[i].position;
         }
-        ExecuteChangePlantLifeTimeJob(preyPositions);
-    }
 
-    public void ExecuteChangePlantLifeTimeJob(Vector3[] preyTransformPosition) 
-    {
-
-        var preyTransformPositionArray = new NativeArray<Vector3>(preyTransformPosition, Allocator.TempJob);
-        var job = new ChangePlantLifeTimeJob
+        ChangePlantLifeTimeJob changePlantLifeTimeJob = new ChangePlantLifeTimeJob
         {
-            preyTransformsPosition = preyTransformPositionArray,
+            preyTransformsPosition = preyTranformPositionArray,
             transformPosition = transform.position,
             touchingDistance = Ex4Config.TouchingDistance,
-            // lifetime = _lifetime,
             decreasingFactor = _lifetime.decreasingFactor
-
-
         };
-        var jobHandle = job.Schedule(preyTransformPositionArray.Length, 64);
+
+        JobHandle jobHandle = changePlantLifeTimeJob.Schedule(Ex4Spawner.PreyTransforms.Length, 64);
         jobHandle.Complete();
-        preyTransformPositionArray.CopyTo(preyTransformPosition);
-        preyTransformPositionArray.Dispose();
+        preyTranformPositionArray.Dispose();
 
     }
+
 
 }
--- TP2/Assets/Ex4/Scripts/ChangePreyLifetime.cs
+++ TP2/Assets/Ex4/Scripts/ChangePreyLifetime.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
 
 public class ChangePreyLifetime : MonoBehaviour
 {
@@ -12,31 +15,104 @@ public class ChangePreyLifetime : MonoBehaviour
     public void Update()
     {
         _lifetime.decreasingFactor = 1.0f;
-        foreach(var plant in Ex4Spawner.PlantTransforms)
+        NativeArray<Vector3> plantTransformPositionArray = new NativeArray<Vector3>(Ex4Spawner.PlantTransforms.Length, Allocator.TempJob);
+        NativeArray<Vector3> predatorTransformPositionArray = new NativeArray<Vector3>(Ex4Spawner.PredatorTransforms.Length, Allocator.TempJob);
+        NativeArray<Vector3> preyTransformPositionArray = new NativeArray<Vector3>(Ex4Spawner.PreyTransforms.Length, Allocator.TempJob);
+        for (int i = 0; i < Ex4Spawner.PlantTransforms.Length; i++)
         {
-            if (Vector3.Distance(plant.position, transform.position) < Ex4Config.TouchingDistance)
-            {
-                _lifetime.decreasingFactor /= 2;
-                break;
-            }
+            plantTransformPositionArray[i] = Ex4Spawner.PlantTransforms[i].position;
         }
-        
-        foreach(var predator in Ex4Spawner.PredatorTransforms)
+        for (int i = 0; i < Ex4Spawner.PredatorTransforms.Length; i++)
         {
-            if (Vector3.Distance(predator.position, transform.position) < Ex4Config.TouchingDistance)
-            {
-                _lifetime.decreasingFactor *= 2f;
-                break;
-            }
+            predatorTransformPositionArray[i] = Ex4Spawner.PredatorTransforms[i].position;
         }
-        
-        foreach(var prey in Ex4Spawner.PreyTransforms)
+        for (int i = 0; i < Ex4Spawner.PreyTransforms.Length; i++)
         {
-            if (Vector3.Distance(prey.position, transform.position) < Ex4Config.TouchingDistance)
-            {
-                _lifetime.reproduced = true;
-                break;
-            }
+            preyTransformPositionArray[i] = Ex4Spawner.PreyTransforms[i].position;
+        }
+
+
+        ChangePreyLifetimePlantTranformeJob changePreyLifetimePlantTranformeJob = new ChangePreyLifetimePlantTranformeJob
+        {
+            plantTransformsPosition = plantTransformPositionArray,
+            transformPosition = transform.position,
+            touchingDistance = Ex4Config.TouchingDistance,
+            decreasingFactor = _lifetime.decreasingFactor
+        };
+        ChangePreyLifetimePredatorTranformeJob changePreyLifetimePredatorTranformeJob = new ChangePreyLifetimePredatorTranformeJob
+        {
+            predatorTransformsPosition = predatorTransformPositionArray,
+            transformPosition = transform.position,
+            touchingDistance = Ex4Config.TouchingDistance,
+            decreasingFactor = _lifetime.decreasingFactor
+        };
+        ChangePreyLifetimePreyTranformeJob changePreyLifetimePreyTranformeJob = new ChangePreyLifetimePreyTranformeJob
+        {
+            preyTransformsPosition = preyTransformPositionArray,
+            transformPosition = transform.position,
+            touchingDistance = Ex4Config.TouchingDistance,
+            reproduced = _lifetime.reproduced
+        };
+
+        JobHandle jobHandlePlant = changePreyLifetimePlantTranformeJob.Schedule(Ex4Spawner.PlantTransforms.Length, 64);
+        JobHandle jobHandlePredator = changePreyLifetimePredatorTranformeJob.Schedule(Ex4Spawner.PredatorTransforms.Length, 64);
+        JobHandle jobHandlePrey = changePreyLifetimePreyTranformeJob.Schedule(Ex4Spawner.PreyTransforms.Length, 64);
+        jobHandlePlant.Complete();
+        jobHandlePredator.Complete();
+        jobHandlePrey.Complete();
+        plantTransformPositionArray.Dispose();
+        predatorTransformPositionArray.Dispose();
+        preyTransformPositionArray.Dispose();
+    }
+}
+
+[BurstCompile]
+struct ChangePreyLifetimePlantTranformeJob : IJobParallelFor
+{
+    public NativeArray<Vector3> plantTransformsPosition;
+    public Vector3 transformPosition;
+    public float touchingDistance;
+    public float decreasingFactor;
+
+    public void Execute(int index)
+    {
+        if (Vector3.Distance(plantTransformsPosition[index], transformPosition) < touchingDistance)
+        {
+            decreasingFactor /= 2;
+        }
+    }
+}
+
+[BurstCompile]
+struct ChangePreyLifetimePredatorTranformeJob : IJobParallelFor
+{
+    public NativeArray<Vector3> predatorTransformsPosition;
+    public Vector3 transformPosition;
+    public float touchingDistance;
+    public float decreasingFactor;
+
+    public void Execute(int index)
+    {
+        if (Vector3.Distance(predatorTransformsPosition[index], transformPosition) < touchingDistance)
+        {
+            decreasingFactor *= 2f;
+        }
+    }
+}
+
+[BurstCompile]
+struct ChangePreyLifetimePreyTranformeJob : IJobParallelFor
+{
+    public NativeArray<Vector3> preyTransformsPosition;
+    public Vector3 transformPosition;
+    public float touchingDistance;
+    public bool reproduced;
+
+    public void Execute(int index)
+    {
+        if (Vector3.Distance(preyTransformsPosition[index], transformPosition) < touchingDistance)
+        {
+            reproduced = true;
         }
     }
 }
