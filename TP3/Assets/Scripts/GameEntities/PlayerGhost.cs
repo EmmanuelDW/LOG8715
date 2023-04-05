9dd6a8990157dae332d3b994314874a1ac2f27d0
Emmanuel Doré-Walsh <emmanuel.dore.walsh@hotmail.com>
2023-04-05 18:58:10 -0400

    atempt de réconciliation  player ghost

--- TP3/Assets/Scripts/GameEntities/PlayerGhost.cs
+++ TP3/Assets/Scripts/GameEntities/PlayerGhost.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -11,19 +12,47 @@ public class PlayerGhost : NetworkBehaviour
     private float _LocalSpeed;
     private Vector2 _LocalPosition;
     private List<Vector2> _LocalPositionBuffer;
+    private NetworkUtility netUtility;
+    private static uint _serverTickRate;
+    private static int _serverTick;
+    private static ulong _RTT;
+    private int _localTickRate;
+    private uint _localTick;
+    private ulong _bufferMax;
+    private int Tolerance;
     
-    
-    
+    private int tempXLocal;
+    private int tempYLocal;
+    private int tempXServer;
+    private int tempYServer;
+    private int diffx;
+    private int diffy;
+
+
+
     [SerializeField] 
     private SpriteRenderer m_SpriteRenderer;
 
     public override void OnNetworkSpawn()
     {
         // L'entite qui appartient au client est recoloriee en rouge
-        _LocalPosition = m_Player.Position;
-        _LocalSpeed = m_Player.m_Velocity;
         
-        if (IsOwner)
+        
+        if (IsClient && IsOwner)
+        {
+            Tolerance = 2;
+            _LocalPosition = m_Player.Position;
+            _LocalSpeed = m_Player.m_Velocity;
+            _localTick = 0;
+            _localTickRate = 60;
+            _serverTick = 0;
+            _serverTickRate = 60;
+            _RTT = 2 + (1 / (ulong)_serverTickRate);
+            _bufferMax = _RTT * (ulong)_localTickRate;
+            
+        }
+
+            if (IsOwner)
         {
             m_SpriteRenderer.color = Color.red;
         }
@@ -31,12 +60,53 @@ public class PlayerGhost : NetworkBehaviour
 
     private void Update()
     {
+        if (IsClient && IsOwner)
+        {
+            _LocalPosition += InputCollecting() * (Time.deltaTime * _LocalSpeed);
+            _LocalPositionBuffer.Add(_LocalPosition);
+            tempXLocal = (int)_LocalPositionBuffer[_LocalPositionBuffer.Count() - 1].x;
+            tempYLocal = (int)_LocalPositionBuffer[_LocalPositionBuffer.Count() - 1].y;
+            tempXServer = (int)m_Player.Position.x;
+            tempYServer = (int)m_Player.Position.y;
+            
+            if ((tempXLocal - Tolerance < tempXServer) && ((tempXLocal + Tolerance > tempXServer))
+                                                       && (tempYLocal - Tolerance < tempYServer) &&
+                                                       ((tempYLocal + Tolerance > tempYServer)))
+            {
+                diffx = tempXServer - tempXLocal ;
+                diffx = tempYServer - tempYLocal ;
+                for (int i = 0 ; i < _LocalPositionBuffer.Count() ; i++)
+                {
+                    var temp = _LocalPositionBuffer[i];
+                    temp.x -= diffx;
+                    temp.y -= diffy;
+                    _LocalPositionBuffer[i] = temp;
+                }
+            }
 
-
-
-        _LocalPosition += InputCollecting();
-        transform.position = _LocalPosition * (Time.deltaTime * _LocalSpeed);
-        //transform.position = m_Player.Position;
+            transform.position = _LocalPositionBuffer[_LocalPositionBuffer.Count()-1];
+            
+            if ((ulong)_LocalPositionBuffer.Count() > _bufferMax)
+            {
+                _LocalPositionBuffer.RemoveAt(0);
+            }
+            
+            
+            
+            
+            
+            
+            _localTick++;
+        }
+        
+        //_LocalPosition += InputCollecting() * (Time.deltaTime * _LocalSpeed);
+        //transform.position = _LocalPosition ;
+        if (IsServer)
+        {
+            transform.position = m_Player.Position;
+        }
+        
+        
     
     
     
